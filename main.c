2a3913f528c8efc1d712b51e1b19d4778c2ec0c1
Muawia24 <amuawia666@gmail.com>
2024-04-06 06:50:08 -0700

    factorize.c

---
+++ main.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * check_args - checks arguments if true it open the file arg
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: file struct
+ */
+FILE *check_args(int argc, char *argv[])
+{
+	FILE *fd;
+
+	if (argc != 2)
+	{
+		dprintf(2, "USAGE: factors <file>\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fd = fopen(argv[1], "r");
+	if (fd == NULL)
+	{
+		dprintf(2, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	return (fd);
+}
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	FILE *fd;
+	char *buf = NULL;
+	size_t size = 0;
+	ssize_t n_line = 0;
+
+	fd = check_args(argc, argv);
+
+	while ((n_line = getline(&buf, &size, fd)) != -1)
+	{
+		is_prime(buf);
+
+	}
+
+	fclose(fd);
+
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define _GNU_SOURCE
+
+void is_prime(char *buf);
+#endif
